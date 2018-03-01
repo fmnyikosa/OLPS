0db41a0b48c7347639308e3c34d720602a7938df
fmnyikosa <fmnyikosa@gmail.com>
2018-03-01 15:26:39 +0000

    more updates

---
+++ Results/hist_APY.m
@@ -0,0 +1,16 @@
+% Draw bar graphs of results
+% Metric: Annualized Percentage Yield (APY)
+%
+% (c) Copyright Favour M Nyikosa <favour@nyikosa.com> 1st March 2018
+
+categories = categorical( ...
+    { 'DJIA', 'SP500', 'TSE', 'MSCI', 'NYSE(O)', 'NYSE(N)' });
+
+results    = {            ...
+    ; ... DJIA
+    ; ... SP500
+    ; ... TSE
+    ; ... MSCI
+    ; ... NYSE(O)
+    ; ... NYSE(N)
+    };
---
+++ Results/hist_VR.m
@@ -0,0 +1,16 @@
+% Draw bar graphs of results
+% Metric: Volatility Risk
+%
+% (c) Copyright Favour M Nyikosa <favour@nyikosa.com> 1st March 2018
+
+categories = categorical( ...
+    { 'DJIA', 'SP500', 'TSE', 'MSCI', 'NYSE(O)', 'NYSE(N)' });
+
+results    = {            ...
+    ; ... DJIA
+    ; ... SP500
+    ; ... TSE
+    ; ... MSCI
+    ; ... NYSE(O)
+    ; ... NYSE(N)
+    };
