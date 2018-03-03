f508ceeee299f18a0205ff0d052487c3da39bfd8
Favour Mandanji Nyikosa <fmnyikosa@gmail.com>
2018-03-03 16:20:48 +0000

    more updates

---
+++ Strategy/initBayesOptData.m
@@ -0,0 +1,10 @@
+% THis function initialises the data for a BayesOpt model
+%
+% Copyright (c) Favour Mandanji Nyikosa <favour@nyikosa.com> 3-MAR-2018
+
+function model = initBayesOptData( x, y, model )
+
+model.xt       = x;
+model.yt       = y;
+
+end
